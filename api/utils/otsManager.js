d803aedd036b25b604766081c75388b976027582
SoleneDc <duchamp.solene.pro@gmail.com>
2018-01-24 10:52:24 +0100

    correction de la fonction verify

--- api/utils/otsManager.js
+++ api/utils/otsManager.js
@@ -11,35 +11,30 @@ var stamp = function (file) {
 
         var detached = OpenTimestamps.DetachedTimestampFile.fromHash(new OpenTimestamps.Ops.OpSHA256(), hashBytes)
         OpenTimestamps.stamp(detached).then(function () {
-            var fileOts = detached.serializeToBytes()
+            var fileOts = detached.serializeToBytes() //TODO: save the file somewhere!
             var infoResult = OpenTimestamps.info(detached)
             console.log(infoResult)
         })
     })
 }
 
-// DOESN'T WORK YET
-//
-// var verify = function (file, fileOts) {
-//     fs.ReadStream(file).on('data', function (data) {
-//         var hasher = crypto.createHash('sha256')
-//         var hash = hasher.update(data).digest('hex')
-//         var hashBytes = OpenTimestamps.Utils.hexToBytes(hash)
-//         var detached = OpenTimestamps.DetachedTimestampFile.fromHash(new OpenTimestamps.Ops.OpSHA256(), hashBytes)
-//
-//         fs.ReadStream('README.md').on('data', function (dataOts) {
-//             console.log(dataOts)
-//             console.log(dataOts instanceof Uint8Array)
-//
-//             var detachedOts = OpenTimestamps.DetachedTimestampFile.deserialize(dataOts)
-//
-//             OpenTimestamps.verify(detachedOts, detached).then(function (verifyResult) {
-//                 console.log(verifyResult)
-//             })
-//         })
-//     })
-// }
+//DOESN'T WORK YET
+
+var verify = function (file, fileOts) {
+    fs.ReadStream(file).on('data', function (data) {
+        var hasher = crypto.createHash('sha256')
+        var hash = hasher.update(data).digest('hex')
+        var hashBytes = OpenTimestamps.Utils.hexToBytes(hash)
+        var detached = OpenTimestamps.DetachedTimestampFile.fromHash(new OpenTimestamps.Ops.OpSHA256(), hashBytes)
+        fs.ReadStream(fileOts).on('data', function (dataOts) {
+            var detachedOts = OpenTimestamps.DetachedTimestampFile.deserialize(dataOts)
+            OpenTimestamps.verify(detachedOts, detached).then(function (verifyResult) {
+                console.log(verifyResult)
+            })
+        })
+    })
+}
 
 module.exports.stamp = stamp
-// module.exports.verify = verify
-// stamp('../../README.md')
+module.exports.verify = verify
+// stamp('../../README2.md')
