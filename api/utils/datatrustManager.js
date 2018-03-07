6c176b9eed9b934a4b6b3c897b395bdb06985baa
Manon <manon.leger@ntymail.com>
2018-03-07 17:26:55 +0100

    Replaced disk storage with memory storage to work only with buffers

--- api/utils/datatrustManager.js
+++ api/utils/datatrustManager.js
@@ -23,73 +23,44 @@ function createUser(email, password) {
     })
 }
 
-// createUser('solene@gmail.com','coucou').then(function (r){console.log(r)})
-
-var stamp = function (file) {
+var stamp = function (buffer) {
     return new Promise(function (resolve) {
-        var rs = fs.ReadStream(file)
-        rs.on('readable', function () {
-            var buffer = rs.read()
-            if (buffer != null) {
-                console.log(buffer)
-                var hash = crypto.createHash('sha256').update(buffer).digest('hex')
-                var url = config.baseUrl + '/stamp'
-                var qs = { api_key: config.api_key }
-                var formData = { digest: hash }
-                request.post({ url: url, qs: qs, formData: formData, json: true }, function (err) {
-                    if (err) { resolve({ success: false, error: err }) }
-                    else { resolve({ success: true, hash: hash }) }
-                })
-            }
+        var hash = crypto.createHash('sha256').update(buffer).digest('hex')
+        var url = config.baseUrl + '/stamp'
+        var qs = { api_key: config.api_key }
+        var formData = { digest: hash }
+        request.post({ url: url, qs: qs, formData: formData, json: true }, function (err) {
+            if (err) { resolve({ success: false, error: err }) }
+            else { resolve({ success: true, hash: hash }) }
         })
     })
 }
 
-// var stamp = function (file) {
-//     return new Promise(function (resolve) {
-//         fs.ReadStream(file).on('data', function (data) {
-//             var buffer = fs.ReadStream(file).on('readable')
-//             var hasher = crypto.createHash('sha256')
-//             var hash = hasher.update(data).digest('hex')
-//             var url = config.baseUrl + '/stamp'
-//             var qs = { api_key: config.api_key }
-//             var formData = { digest: hash }
-//             request.post({ url: url, qs: qs, formData: formData, json: true }, function (err) {
-//                 if (err) { resolve({ success: false, error: err }) }
-//                 else { resolve({ success: true , hash: hash}) }
-//                 // else { resolve({ success: true }) }
-//             })
-//         })
-//     })
-// }
-
-var verify = function (file) {
+var verify = function (buffer) {
     return new Promise(function (resolve) {
-        fs.ReadStream(file).on('data', function (data) {
-            var hash = crypto.createHash('sha256').update(data).digest('hex')
-            var url = config.baseUrl + '/stamp'
-            var qs = { api_key: config.api_key }
-            var formData = { digest: hash }
-            request.get({ url: url, qs: qs, formData: formData, json: true }, function (err, resp, body) {
-                if (err) { resolve({ success: false, error: err }) }
-                else {
-                    resolve({ success: true, result: body[0].status })
+        var hash = crypto.createHash('sha256').update(buffer).digest('hex')
+        var url = config.baseUrl + '/stamp'
+        var qs = { api_key: config.api_key }
+        var formData = { digest: hash }
+        request.get({ url: url, qs: qs, formData: formData, json: true }, function (err, resp, body) {
+            if (err) { resolve({ success: false, error: err }) }
+            else {
+                resolve({ success: true, result: body[0].status })
 
-                    // === A ré-activer quand la carte 49 sera effectuée ===
-                    //
-                    // Agent.findOne(
-                    //     { accountId: body[0].account_id },
-                    //     function (err, agent) {
-                    //         if (err) { resolve({ success: false, error: err }) }
-                    //         else if (agent === null) {
-                    //             resolve({success: false, error: 'No corresponding agent was found.'})
-                    //         }
-                    //         else if (agent.apiKey === qs.api_key) {
-                    //             resolve({ success: true, result: body.array[0].status })
-                    //         }
-                    //     })
-                }
-            })
+                // === A ré-activer quand la carte 49 sera effectuée ===
+                //
+                // Agent.findOne(
+                //     { accountId: body[0].account_id },
+                //     function (err, agent) {
+                //         if (err) { resolve({ success: false, error: err }) }
+                //         else if (agent === null) {
+                //             resolve({success: false, error: 'No corresponding agent was found.'})
+                //         }
+                //         else if (agent.apiKey === qs.api_key) {
+                //             resolve({ success: true, result: body.array[0].status })
+                //         }
+                //     })
+            }
         })
     })
 }
